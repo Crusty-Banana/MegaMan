1b90ba7862739af90988c0fbef98485e78aa2256
JackSmart1234 <dodoankhac@gmail.com>
2023-06-04 13:41:40 +0700

    testing

--- Megaman.py
+++ Megaman.py
@@ -1,2 +1,18 @@
 import retro
+
 env = retro.make(game='MegaMan2-Nes')
+
+env.reset()
+
+done = False
+
+while not done:
+    env.render()
+
+    action = env.action_space.sample()
+
+    screen, reward, done, info = env.step(action)
+
+    print("Action", action)
+    print("image ", screen.shape, "reward ", reward, "Done?", done)
+    print("Info", info)
