38eee3bdf6a213c377147abe2ce037c5d7523b31
JackSmart1234 <dodoankhac@gmail.com>
2023-06-05 23:32:17 +0700

    added openCV module and implment find_contour and default policy for testing

--- Megaman.py
+++ Megaman.py
@@ -1,18 +1,58 @@
 import retro
-
+import random
 env = retro.make(game='MegaMan2-Nes')
 
 env.reset()
 
 done = False
 
+
+frame_count_shooting = 0
+frame_count_jumping = 0
+def default_policy():
+    #[shoot, dunno, dunno, up?, down?, left, right, jump]
+    default_policy = [0, 0, 0, 0, 0, 0, 0, 1, 0]
+    global frame_count_shooting, frame_count_jumping
+    frame_count_shooting += 1
+    if (frame_count_shooting == 10):
+        frame_count_shooting = 0
+        default_policy[0] = 1
+
+    frame_count_jumping += 1
+    if (frame_count_jumping > 30):
+        default_policy[-1] = 1
+    if (frame_count_jumping == 60):
+        frame_count_jumping = 0
+
+    return default_policy
+
+import cv2
+import numpy as np
+def find_contour(img):
+    #convert img to grey
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #set a thresh
+    thresh = 100
+    #get threshold image
+    ret,thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
+    #find contours
+    contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #create an empty image for contours
+    img_contours = np.zeros(img.shape)
+    # draw the contours on the empty image
+    cv2.drawContours(img_contours, contours, -1, (0,255,0), 3)
+    #save image
+    cv2.imwrite('D:/contours.png',img_contours)
+    return
+
 while not done:
     env.render()
 
     action = env.action_space.sample()
 
+    action = default_policy()
     screen, reward, done, info = env.step(action)
-
+    find_contour(screen)
     print("Action", action)
     print("image ", screen.shape, "reward ", reward, "Done?", done)
     print("Info", info)
--- READ-ME.md
+++ READ-ME.md
@@ -5,3 +5,24 @@ python -m venv .venv
 pip install gym==0.21.0
 python -m retro.import .
 ```
+
+Todo:
+1. Define what is in a state
+2. Define action space
+3. Define reward 
+4. Established learning rate $\alpha$
+5. Established discounting factor $\gamma$ 
+6. Established initial policy $\pi$ 
+7. Established exploration strategy
+
+<h2> I. Definition of a State</h2>
+
+- Health: How much health Mega Man have left $[0, 24]$
+- Lives: How many death can Mega Man have before Game Over $[0, 3]$
+- Progress: How much progress have he make, or how far have he gone $[0, +\infty]$
+- The screen: pixels on the screen at the current time.
+
+<h2> III. Initial policy</h2>
+
+move right and shoot
+   
